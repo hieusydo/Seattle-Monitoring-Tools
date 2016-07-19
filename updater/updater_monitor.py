c33d7f2cad8625c32e7887194714e623b668d5dd
hieusydo <hieusydo@gmail.com>
2016-07-19 17:27:18 -0400

    add more comments

--- updater/updater_monitor.py
+++ updater/updater_monitor.py
@@ -5,8 +5,9 @@ Author: Hieu Do
 Start Date: June 6, 2016
 
 Description: 
-Retrieves a list of all the files from `https://seattle.poly.edu/updatesite/` 
-and returns the files as well as their last modified dates.
+Check the status of the updater server 
+and if the updater site delivers the correct updates 
+by checking the signatures in a downloaded metainfo
 
 Usage: 
 python updater_monitor.py
@@ -26,12 +27,14 @@ advertise_monitor = dy_import_module("advertise_monitor.r2py")
 updatereurl = "https://seattle.poly.edu/updatesite/"
 
 updater_servername = "seattle.poly.edu"
-updater_serverport = 443 # port obtain from `nmap 128.238.63.51`
+updater_serverport = 443 # https port obtain from `nmap 128.238.63.51`
 
 
 def main():
+  # check the status of the advertise server
   advertise_monitor.checkServerStatus(updater_servername, updater_serverport)
 
+  # download a temp 'metainfo' file
   tempdir = tempfile.mkdtemp()+"/"
   metainfo_downloaded = softwareupdater.safe_download(updatereurl, "metainfo", tempdir, 1024*32)
   if not metainfo_downloaded:
@@ -44,11 +47,14 @@ def main():
   newmetafiledata = newmetafileobject.read()
   newmetafileobject.close()
 
+  # check the signature of the downloaded 'metainfo'
+  # if the signature in the file matches with the software update public key, 
+  # then the software updater is delivering the correct update
   if not signeddata.signeddata_issignedcorrectly(newmetafiledata, softwareupdater.softwareupdatepublickey):
     print("CRITICAL - Downloaded metainfo not signed correctly. The updater site is delivering the correct updates")
     return
   print("Downloaded metainfo signed correctly. The updater site is delivering the correct updates")
-  
+
 
 if __name__ == '__main__':
   main()
